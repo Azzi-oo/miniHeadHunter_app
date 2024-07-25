d29637c97a9b1fb591d3bb24cc1e906c7e62ad03
Azzi-oo <yanberdin.az@yandex.ru>
2024-07-25 15:12:13 +0500

    add new JOIN, SELECT queries

--- src/queries/orm.py
+++ src/queries/orm.py
@@ -77,3 +77,76 @@ class SyncORM:
                 .having(cast(func.avg(ResumesOrm.compensation), Integer) > 70000)
             )
             res = session.execute(query)
+
+    @staticmethod
+    def insert_additional_resumes():
+        with sync_engine.connect() as conn:
+            workers = [
+                {"username": "Artem"},  # id 3
+                {"username": "Roman"},  # id 4
+                {"username": "Petr"},   # id 5
+            ]
+            resumes = [
+                {"title": "Python программист", "compensation": 60000, "workload": "fulltime", "worker_id": 3},
+                {"title": "Machine Learning Engineer", "compensation": 70000, "workload": "parttime", "worker_id": 3},
+                {"title": "Python Data Scientist", "compensation": 80000, "workload": "parttime", "worker_id": 4},
+                {"title": "Python Analyst", "compensation": 90000, "workload": "fulltime", "worker_id": 4},
+                {"title": "Python Junior Developer", "compensation": 100000, "workload": "fulltime", "worker_id": 5},
+            ]
+            insert_workers = insert(workers_table).values(workers)
+            insert_resumes = insert(resumes_table).values(resumes)
+            conn.execute(insert_workers)
+            conn.execute(insert_resumes)
+            conn.commit()
+
+    @staticmethod
+    def join_cte_subquery_window_func():
+        """
+        WITH helper2 AS (
+            SELECT *, compensation-avg_workload_compensation AS compensation_diff
+            FROM
+            (SELECT
+                w.id,
+                w.username,
+                r.compensation,
+                r.workload,
+                avg(r.compensation) OVER (PARTITION BY workload)::int AS avg_workload_compensation
+            FROM resumes r
+            JOIN workers w ON r.worker_id = w.id) helper1
+        )
+        SELECT * FROM helper2
+        ORDER BY compensation_diff DESC;
+        """
+        with sync_engine.connect() as conn:
+            r = aliased(resumes_table)
+            w = aliased(workers_table)
+            subq = (
+                select(
+                    r,
+                    w,
+                    func.avg(r.c.compensation).over(partition_by=r.c.workload).cast(Integer).label("avg_workload_compensation"),
+                )
+                # .select_from(r)
+                .join(r, r.c.worker_id == w.c.id).subquery("helper1")
+            )
+            cte = (
+                select(
+                    subq.c.worker_id,
+                    subq.c.username,
+                    subq.c.compensation,
+                    subq.c.workload,
+                    subq.c.avg_workload_compensation,
+                    (subq.c.compensation - subq.c.avg_workload_compensation).label("compensation_diff"),
+                )
+                .cte("helper2")
+            )
+            query = (
+                select(cte)
+                .order_by(cte.c.compensation_diff.desc())
+            )
+
+            res = conn.execute(query)
+            result = res.all()
+            print(f"{len(result)=}. {result=}")
+
+    # Relationships отсутствуют при использовании Table
